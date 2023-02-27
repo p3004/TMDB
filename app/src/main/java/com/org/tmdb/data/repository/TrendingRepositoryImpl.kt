ee2460b7faa9cf649548fb696d9843c6078d4226
pallab <pallab.115045@gmail.com>
2023-02-27 15:18:32 +0530

    "refactor: Added withContext(Dispatchers.IO) to run repository api call in IO thread of coroutines"

--- app/src/main/java/com/org/tmdb/data/repository/TrendingRepositoryImpl.kt
+++ app/src/main/java/com/org/tmdb/data/repository/TrendingRepositoryImpl.kt
@@ -2,20 +2,24 @@ package com.org.tmdb.data.repository
 
 import com.org.tmdb.data.remote.ApiCalls
 import com.org.tmdb.data.remote.Trending
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class TrendingRepositoryImpl @Inject constructor(
     private val apiCalls: ApiCalls
-): TrendingRepository {
+) : TrendingRepository {
 
     override suspend fun fetchTrendingData(
         mediaType: String,
         timeWindow: String
     ): Flow<Trending> {
-        return apiCalls.getTrendingData(
-            mediaType = mediaType,
-            timeWindow = timeWindow
-        )
+        return withContext(Dispatchers.IO) {
+            apiCalls.getTrendingData(
+                mediaType = mediaType,
+                timeWindow = timeWindow
+            )
+        }
     }
 }
