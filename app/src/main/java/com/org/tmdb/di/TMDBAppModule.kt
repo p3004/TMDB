30ab688df17f2f4ff6e0151592bc842e6aa0cb96
pallab <pallab.115045@gmail.com>
2023-02-27 14:34:59 +0530

    "internal: Added MainViewModel.kt and provided trendingRepository dependency via TMDBAppModule.kt with Hilt"

--- app/src/main/java/com/org/tmdb/di/TMDBAppModule.kt
+++ app/src/main/java/com/org/tmdb/di/TMDBAppModule.kt
@@ -2,6 +2,8 @@ package com.org.tmdb.di
 
 import com.org.tmdb.data.remote.ApiCalls
 import com.org.tmdb.data.remote.RetrofitObjectCreator
+import com.org.tmdb.data.repository.TrendingRepository
+import com.org.tmdb.data.repository.TrendingRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,5 +18,9 @@ object TMDBAppModule {
     @Singleton
     @Provides
     fun provideApiCall(): ApiCalls = RetrofitObjectCreator.create()
+
+    @Provides
+    @Singleton
+    fun provideTrendingRepository(apiCalls: ApiCalls) : TrendingRepository = TrendingRepositoryImpl(apiCalls)
 
 }
---
+++ app/src/main/java/com/org/tmdb/ui/viewmodels/MainViewModel.kt
@@ -0,0 +1,47 @@
+package com.org.tmdb.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.org.tmdb.data.remote.Trending
+import com.org.tmdb.data.repository.TrendingRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val trendingRepository: TrendingRepository
+) : ViewModel() {
+
+    private val _trendingStateFlow =
+        MutableStateFlow<MainActivityUiState>(MainActivityUiState.Loading)
+
+    val trendingStateFlow: StateFlow<MainActivityUiState>
+        get() = _trendingStateFlow
+
+    /**
+     * stores [MainActivityUiState] into [_trendingStateFlow]
+     * */
+    fun fetchTrendingData(mediaType: String, timeWindow: String) {
+        viewModelScope.launch {
+            _trendingStateFlow.value = MainActivityUiState.Loading
+           trendingRepository.fetchTrendingData(
+                mediaType, timeWindow
+            ).catch {
+                _trendingStateFlow.value =
+                    MainActivityUiState.Error(it.message ?: "Something went wrong")
+            }.collect {
+                _trendingStateFlow.value = MainActivityUiState.Success(it)
+            }
+        }
+    }
+}
+
+sealed interface MainActivityUiState {
+    object Loading : MainActivityUiState
+    data class Success(val trendingStateFlow: Trending) : MainActivityUiState
+    data class Error(val errorMsg: String) : MainActivityUiState
+}
