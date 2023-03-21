bd4aa278f0b8109c1a50912673ddaf9a5c943867
pallab <pallab.115045@gmail.com>
2023-03-22 00:29:17 +0530

    "internal: Added kotlin coroutines test dependencies"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,10 @@ dependencies {
     //Mockito
     testImplementation("org.mockito:mockito-core:3.5.10")
 
+    //Kotlin test dependencies
+    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
