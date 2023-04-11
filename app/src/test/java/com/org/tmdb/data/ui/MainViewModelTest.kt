e813c9fa4a2e01f339726c4107b201023b598f71
pallab <pallab.115045@gmail.com>
2023-04-11 22:28:21 +0530

    "test: Added Unit tests for MainViewModel with test coverage of 66%"

--- app/src/test/java/com/org/tmdb/data/ui/MainViewModelTest.kt
+++ app/src/test/java/com/org/tmdb/data/ui/MainViewModelTest.kt
@@ -1,15 +1,20 @@
 package com.org.tmdb.data.ui
 
+import com.org.tmdb.data.remote.errorMessage
+import com.org.tmdb.data.remote.trendingTestData
 import com.org.tmdb.data.repository.TrendingRepository
 import com.org.tmdb.data.util.TestDispatcherRule
 import com.org.tmdb.ui.viewmodels.MainActivityUiState
 import com.org.tmdb.ui.viewmodels.MainViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 import kotlin.test.assertEquals
 
@@ -42,13 +47,31 @@ class MainViewModelTest {
 
     @Test
     fun `when server response 200 should fetch trending data`(){
-        
+        runBlocking {
+            doReturn(trendingTestData)
+                .`when`(trendingRepository)
+                .fetchTrendingData("","")
+
+            mainViewModel.fetchTrendingData("", "")
+
+            verify(trendingRepository).fetchTrendingData("","")
+        }
     }
 
 
     @Test
-    fun `when server`(){
+    fun `when server response ERROR should return error message`(){
+        runBlocking {
+         doThrow(RuntimeException(errorMessage))
+             .`when`(trendingRepository)
+             .fetchTrendingData("","")
 
+         mainViewModel.fetchTrendingData("","")
+
+         verify(trendingRepository).fetchTrendingData("","").catch {
+             assertEquals(it.message, errorMessage)
+         }
+        }
     }
 
 }
