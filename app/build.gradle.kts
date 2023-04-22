8679bf2d2783b78812edc69023813262e451d076
pallab <pallab.115045@gmail.com>
2023-04-22 21:31:10 +0530

    "chore: Updated gradle dependencies"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.2"
     }
-    packagingOptions {
+    packaging {
         resources {
             excludes.add("/META-INF/{AL2.0,LGPL2.1}")
         }
@@ -63,10 +63,10 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.10.0")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation ("androidx.activity:activity-compose:1.7.0")
-    implementation ("androidx.compose.ui:ui:1.4.1")
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.4.1")
-    implementation ("androidx.compose.material:material:1.4.1")
+    implementation ("androidx.activity:activity-compose:1.7.1")
+    implementation ("androidx.compose.ui:ui:1.4.2")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.4.2")
+    implementation ("androidx.compose.material:material:1.4.2")
     //Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
@@ -86,9 +86,9 @@ dependencies {
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.4.1")
-    debugImplementation ("androidx.compose.ui:ui-tooling:1.4.1")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.4.1")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.4.2")
+    debugImplementation ("androidx.compose.ui:ui-tooling:1.4.2")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.4.2")
 }
 
 kapt {
