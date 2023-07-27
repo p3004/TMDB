9e2bacd2a0d96f1fe6ef3076ad5f6dfff9f1e311
pallab <pallab.115045@gmail.com>
2023-07-28 00:06:36 +0530

    "feat: used systemUIController to control the system's status bar to match the current theme"

--- app/src/main/java/com/org/tmdb/ui/MainActivity.kt
+++ app/src/main/java/com/org/tmdb/ui/MainActivity.kt
@@ -3,12 +3,15 @@ package com.org.tmdb.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.org.tmdb.data.remote.MediaType
 import com.org.tmdb.data.remote.TimeWindow
 import com.org.tmdb.ui.navigation.TMDBNavHost
@@ -31,6 +34,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val systemUiController = rememberSystemUiController()
+            val userDarkIcons = !isSystemInDarkTheme()
+            DisposableEffect(systemUiController, userDarkIcons) {
+                systemUiController.systemBarsDarkContentEnabled = !userDarkIcons
+                onDispose {}
+            }
             TMDBTheme {
                 // A surface container using the 'background' color from the theme
                 TMDBApp(appState = rememberTMDBAppState(networkObserver = networkObserver))
